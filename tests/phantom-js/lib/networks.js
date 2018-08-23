7fc5cfac94694726644f928f8f32204966f2424a
alokin1337 <tr23a1kjko@gmail.com>
2018-08-23 18:47:07 +0200

    rename

--- tests/phantom-js/lib/networks.js
+++ tests/phantom-js/lib/networks.js
@@ -2,20 +2,20 @@ module.exports = {
   phantom: {
     messagePrefix: '\x18Phantom Signed Message:\n',
     bip32: {
-      public: 0x2bf4968, // base58 will have a prefix 'apub'
-      private: 0x2bf4530 // base58Priv will have a prefix 'apriv'
+      public: 0x43587CF      , // base58 will have a prefix 'apub'
+      private: 0x4358394 // base58Priv will have a prefix 'apriv'
     },
     pubKeyHash: 0x37, // Addresses will begin with 'A'
     wif: 0xaa // Network prefix for wif generation
   },
   testnet: {
-    messagePrefix: '\x18Phantom Testnet Signed Message:\n',
+    messagePrefix: '\x18Phantom Signed Message:\n',
     bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
+      public: 0x43587CF      , // base58 will have a prefix 'apub'
+      private: 0x4358394 // base58Priv will have a prefix 'apriv'
     },
-    pubKeyHash: 0x52, // Addresses will begin with 'a'
-    wif: 0xba // Network prefix for wif generation
+    pubKeyHash: 0x37, // Addresses will begin with 'A'
+    wif: 0xaa // Network prefix for wif generation
   },
   bitcoin: {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
@@ -27,12 +27,12 @@ module.exports = {
     wif: 0x80
   },
 phantomTestnet: {
-  messagePrefix: "\x18PHANTOM Testnet message:\n",
+  messagePrefix: '\x18Phantom Signed Message:\n',
   bip32: {
-    public: 70617039,
-    private: 70615956
+    public: 0x43587CF      , // base58 will have a prefix 'apub'
+    private: 0x4358394 // base58Priv will have a prefix 'apriv'
   },
-  pubKeyHash: 55,
-  wif: 187,
+  pubKeyHash: 0x37, // Addresses will begin with 'A'
+  wif: 0xaa // Network prefix for wif generation
 }
 }
